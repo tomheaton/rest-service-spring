0572176b864786683b7ebb590e6e38d369c6916a
Thomas Heaton <thomas.m.heaton@gmail.com>
2021-08-17 22:12:48 +0100

    added new deps, updated spring version, java compat version, renamed project

--- src/test/java/com/tomheaton/restservice/RestServiceApplicationTests.java
+++ src/test/java/com/tomheaton/restservice/RestServiceApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class RestServiceApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
